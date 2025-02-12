8afe22583dc3ee18e8383be79dca0fe577922b75
Marcelo XP <marceloxp@gmail.com>
2025-02-11 22:27:59 -0300

    - Adjust footer.

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
             <Projects :projects="data.projects" />
         </section>
 
-        <section v-if="data" class="mt-10">
+        <section v-if="data" class="mt-10 mb-10">
             <h1 class="text-2xl font-semibold text-center mb-8">{{ data.labels.journey }}</h1>
             <ul v-for="(tech, index) in data.history" :key="tech.period"
                 class="timeline timeline-snap-icon max-lg:timeline-compact timeline-vertical">
@@ -48,23 +48,21 @@
             </ul>
         </section>
 
-        <footer
-            class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4 mt-10 text-gray-500">
-            <aside class="grid-flow-col items-center">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M3 8.90027C4.62072 10.7352 8.0418 12 12 12C15.9582 12 19.3793 10.7352 21 8.90027M3 8.90027V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.90027M3 8.90027V8.2C3 7.07989 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569 5.40973 4.09202 5.21799C4.51984 5 5.07989 5 6.2 5H17.8C18.9201 5 19.4802 5 19.908 5.21799C20.2843 5.40973 20.5903 5.71569 20.782 6.09202C21 6.51984 21 7.07989 21 8.2V8.90027"
-                        stroke="#e7e3e4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-                <p>{{ data.labels.contact }}: <a href="mailto:marceloxp@gmail.com"
-                        class="text-blue-500">marceloxp@gmail.com</a>
-                </p>
-            </aside>
-            <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-                <NuxtLink v-if="lang === 'en'" to="/pt" class="text-xl">🇧🇷</NuxtLink>
-                <NuxtLink v-if="lang === 'pt'" to="/" class="text-xl">🇺🇸</NuxtLink>
-            </nav>
-
+        <footer class="rounded-b-lg p-5">
+            <div class="grid grid-cols-1 sm:grid-cols-[auto_1fr] gap-4 w-6/12 mx-auto">
+                <div class="flex justify-center sm:justify-start">
+                    <p><a href="mailto:marceloxp@gmail.com" class="text-blue-500">marceloxp@gmail.com</a>
+                    </p>
+                </div>
+                <div class="text-center md:text-right">
+                    <p>
+                        <NuxtLink v-if="lang === 'en'" to="/pt" class="tooltip text-xl" data-tip="Mudar Linguagem">🇧🇷
+                        </NuxtLink>
+                        <NuxtLink v-if="lang === 'pt'" to="/" class="tooltip text-xl" data-tip="Change Language">🇺🇸
+                        </NuxtLink>
+                    </p>
+                </div>
+            </div>
         </footer>
     </div>
 </template>
@@ -101,5 +99,9 @@ body {
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+}
+
+footer {
+    background-color: #27252573;
 }
 </style>
